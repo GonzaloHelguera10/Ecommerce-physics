82e0dc465ff1f54148395e263bfb116b538f14e1
Facundo Alvarado <fa.alvarado777@gmail.com>
2024-06-27 12:45:59 -0300

    add images and file js of the navbar

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//Seccion navbar facundo
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+
 // inicio del formulario de frijol
 
 const url = 'https://667a0a1018a459f639522931.mockapi.io/contact';
