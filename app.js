3b09115fc25481b961299c042150c2645864d7c9
GonzaloHelguera10 <helgueragonzalo10@gmail.com>
2024-06-28 10:51:35 -0300

    fix error in code

--- app.js
+++ app.js
@@ -2,24 +2,25 @@
 
 const url = 'https://667a0a1018a459f639522931.mockapi.io/contact';
 const form = document.getElementById('contactForm');
+
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('btn');
-    
+
     btn.addEventListener('click', async function (event) {
         event.preventDefault();
-        
+
         // Obtengo los datos del formulario
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const message = document.getElementById('message').value;
-        
+
         // Creo el objeto de datos
         const contactData = {
             name: name,
             email: email,
-            message: message
+            message: message,
         };
-        
+
         try {
             // Envío los datos al endpoint usando fetch
             const response = await fetch(url, {
@@ -33,6 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
             // Proceso la respuesta del servidor
             const result = await response.json();
             console.log('Respuesta del servidor:', result);
+        } catch (error) {
+            console.error('Error al enviar los datos:', error);
+        }
     });
 });
+
 // fin del formulario
