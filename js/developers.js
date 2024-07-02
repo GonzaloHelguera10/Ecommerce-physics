2af98666dec1aa8d866a6f90c5e5eaecdd8fcd92
GonzaloHelguera10 <helgueragonzalo10@gmail.com>
2024-07-02 15:44:54 -0300

    fix reponsive developer/card

--- js/developers.js
+++ js/developers.js
@@ -1,23 +1,27 @@
+// URL de la API
 const urlDevelopers = "https://6682c8534102471fa4c83a78.mockapi.io/developers";
 
-// Función para cargar los datos de los desarrolladores desde la API
+// Función para obtener los desarrolladores desde la API
 async function fetchDevelopers() {
     try {
         const response = await fetch(urlDevelopers);
         if (!response.ok) {
-            throw new Error('La solicitud HTTP ha fallado');
+            throw new Error('Error al obtener los datos');
         }
-        const data = await response.json();
-        updateDeveloperCards(data);
+        const developers = await response.json();
+        updateDeveloperCards(developers);
     } catch (error) {
-        console.error('Error al obtener datos de los desarrolladores:', error);
+        console.error('Error al obtener los desarrolladores:', error);
     }
 }
+
 // Función para actualizar las tarjetas de desarrolladores con los datos obtenidos
 function updateDeveloperCards(developers) {
     const developerCards = document.querySelectorAll('.developer-card');
     developerCards.forEach((card, index) => {
         const developer = developers[index];
+        if (!developer) return; // Si no hay suficientes desarrolladores, salir de la función
+
         const name = developer.name;
         const surname = developer.surname;
         const github = developer.github;
@@ -33,8 +37,7 @@ function updateDeveloperCards(developers) {
 
         const mailLink = card.querySelector('.contact-link:nth-of-type(2)');
         mailLink.href = "#"; // No redirigir a ninguna parte
-        mailLink.target = ""; // No abrir en una nueva pestaña
-        mailLink.innerHTML = `<i class='bx bx-mail-send icon'></i> <span class="small-text">${mail}</span>`; // Texto de correo con clase
+        mailLink.innerHTML = `<i class='bx bx-mail-send icon'></i> ${mail}`; // Mostrar el correo en el texto
     });
 }
 
